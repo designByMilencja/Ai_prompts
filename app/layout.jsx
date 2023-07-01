516fe14948ec12341846543152c8364b498d9bba
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-01 14:31:47 +0200

    feat(layout): added layout and metadata

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/global.css';
+import Nav from "@components/Nav";
+
+export const metadata = {
+    title: ' Promptopia',
+    description: 'Discover and share AI Prompts'
+}
+const RootLayout = ({children}) => {
+    return (
+        <html lang="en">
+        <body>
+        <div className="main">
+            <div className="gradient"></div>
+        </div>
+        <main className="app">
+            <Nav/>
+            {children}
+        </main>
+        </body>
+        </html>
+    )
+}
+export default RootLayout
