8475874e28427f69ffee16bbfc0ec761f61663a7
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-03 18:56:40 +0200

    feat(PromptCard): added properties of prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,4 +1,25 @@
-const PromptCard = () => {
-    return (<div>PromptCard</div>)
+"use client"
+import {useState} from "react";
+import Image from "next/image";
+import {useSession} from "next-auth/react";
+import {usePathname, useRouter} from "next/navigation";
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+    const [copied, setCopied] = useState("");
+    return (<div className="prompt_card">
+        <div className="flex justify-between items-start gap-5">
+            <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+                <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain"/>
+                <div className="flex flex-col ">
+                    <h3 className="font-semibold font-satoshi text-gray-900">{post.creator.username}</h3>
+                    <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+                </div>
+            </div>
+            <div className="copy_btn" onClick={()=> {}}>
+                <Image width={12} height={12} src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}/>
+            </div>
+        </div>
+        <p className="m-4 font-satoshi text-gray-700 text-sm">{post.prompt}</p>
+        <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={()=>handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+    </div>)
 }
 export default PromptCard
